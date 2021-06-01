e574a3f189ba6326dc2d7bdb14677922a756b982
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2021-06-01 12:04:25 +0800

    load local image

--- app/src/main/java/sg/edu/rp/c347/id19007966/demoaddon/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c347/id19007966/demoaddon/MainActivity.java
@@ -3,12 +3,18 @@ package sg.edu.rp.c347.id19007966.demoaddon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+        imageView.setImageResource(R.mipmap.ic_launcher);
     }
 }
